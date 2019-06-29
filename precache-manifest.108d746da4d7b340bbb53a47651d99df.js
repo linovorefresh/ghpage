0d71cac96b4a4ea554afcaac069cfb8d66889c47
Lino Patulot <linolp@gmail.com>
2019-06-29 06:16:24 -0700

    Updates

---
+++ precache-manifest.108d746da4d7b340bbb53a47651d99df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ccd67c787eef27ec047ed3fdee27ac",
+    "url": "/ghpage/index.html"
+  },
+  {
+    "revision": "efd5e4def85479134819",
+    "url": "/ghpage/static/css/main.b71858d0.chunk.css"
+  },
+  {
+    "revision": "de696eeab47243b14282",
+    "url": "/ghpage/static/js/2.d57aaec3.chunk.js"
+  },
+  {
+    "revision": "efd5e4def85479134819",
+    "url": "/ghpage/static/js/main.78568811.chunk.js"
+  },
+  {
+    "revision": "99a086fbe0561426e1c9",
+    "url": "/ghpage/static/js/runtime~main.3a099b45.js"
+  }
+]);
